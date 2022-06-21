fa96b639e98863bfbe5b00c0189bbf0a9cf67120
Martin Miscin <martin.miscin@sartura.hr>
2022-06-21 10:14:51 +0200

    Proširen logread

--- owrtinfo-src/owrtinfo.sh
+++ owrtinfo-src/owrtinfo.sh
@@ -13,15 +13,15 @@ do
 	fi
 	if [ $kernel == 1 ]; then 
 		kernel1=$(ubus call system board | jsonfilter -e '@["kernel"]')
-		logger_output="$(logger_output); kernel: $kernel1"
+		logger_output="${logger_output}; kernel: $kernel1"
 	fi
 	if [ $openwrt_version == 1 ]; then 
 		openwrt_version1=$(ubus call system board | jsonfilter -e '@["release"]["description"]')
-		logger_output=" $(logger_output); openwrt_version: $openwrt_version1"
+		logger_output=" ${logger_output}; openwrt_version: $openwrt_version1"
 	fi
 	if [ $wanip == 1 ]; then
 		wanip1=$(uci get network.wan.ipaddr)
-		logger_output=" $(logger_output); wanip: $wanip1"
+		logger_output=" ${logger_output}; wanip: $wanip1"
 	fi
 	if [ "$logger_output" != "$logger_output1" ]; then
 		logger -t owrtinfo "$logger_output"
