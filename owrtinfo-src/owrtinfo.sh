4b1c83a71b7e771a3f355c7499221019c09731af
Martin Miscin <martin.miscin@sartura.hr>
2022-06-23 23:44:33 +0200

    Ispravljen case statement

--- owrtinfo-src/owrtinfo.sh
+++ owrtinfo-src/owrtinfo.sh
@@ -24,75 +24,75 @@ do
 	if [ $wanip == 1 ]; then
 		wanip1=$(uci get network.wan.ipaddr)
 		logger_output=" ${logger_output}; wanip: $wanip1"
+		case "$netmask" in
+			"0.0.0.0") logger_output=" ${logger_output}/0 "
+			;;
+			"128.0.0.0") logger_output=" ${logger_output}/1 "
+			;;
+			"192.0.0.0") logger_output=" ${logger_output}/2 "
+			;;
+			"224.0.0.0") logger_output=" ${logger_output}/3 "
+			;;
+			"240.0.0.0") logger_output=" ${logger_output}/4 "
+			;;
+			"248.0.0.0") logger_output=" ${logger_output}/5 "
+			;;
+			"252.0.0.0") logger_output=" ${logger_output}/6 "
+			;;
+			"254.0.0.0") logger_output=" ${logger_output}/7 "
+			;;
+			"255.0.0.0") logger_output=" ${logger_output}/8 "
+			;;
+			"255.128.0.0") logger_output=" ${logger_output}/9 "
+			;;
+			"255.192.0.0") logger_output=" ${logger_output}/10 "
+			;;
+			"255.224.0.0") logger_output=" ${logger_output}/11 "
+			;;
+			"255.240.0.0") logger_output=" ${logger_output}/12 "
+			;;
+			"255.258.0.0") logger_output=" ${logger_output}/13 "
+			;;
+			"255.252.0.0") logger_output=" ${logger_output}/14 "
+			;;
+			"255.254.0.0") logger_output=" ${logger_output}/15 "
+			;;
+			"255.255.0.0") logger_output=" ${logger_output}/16 "
+			;;
+			"255.255.128.0") logger_output=" ${logger_output}/17 "
+			;;
+			"255.255.192.0") logger_output=" ${logger_output}/18 "
+			;;
+			"255.255.224.0") logger_output=" ${logger_output}/19 "
+			;;
+			"255.255.240.0") logger_output=" ${logger_output}/20 "
+			;;
+			"255.255.248.0") logger_output=" ${logger_output}/21 "
+			;;
+			"255.255.252.0") logger_output=" ${logger_output}/22 "
+			;;
+			"255.255.254.0") logger_output=" ${logger_output}/23 "
+			;;
+			"255.255.255.0") logger_output=" ${logger_output}/24 "
+			;;
+			"255.255.255.128") logger_output=" ${logger_output}/25 "
+			;;
+			"255.255.255.192") logger_output=" ${logger_output}/26 "
+			;;
+			"255.255.255.224") logger_output=" ${logger_output}/27 "
+			;;
+			"255.255.255.240") logger_output=" ${logger_output}/28 "
+			;;
+			"255.255.255.248") logger_output=" ${logger_output}/29 "
+			;;
+			"255.255.255.252") logger_output=" ${logger_output}/30 "
+			;;
+			"255.255.255.254") logger_output=" ${logger_output}/31 "
+			;;
+			"255.255.255.255") logger_output=" ${logger_output}/32 "
+			;;		
+		esac
 	fi
-	case "$netmask" in
-		"0.0.0.0") logger_output=" ${logger_outout}/0 "
-		;;
-		"128.0.0.0") logger_output=" ${logger_outout}/1 "
-		;;
-		"192.0.0.0") logger_output=" ${logger_outout}/2 "
-		;;
-		"224.0.0.0") logger_output=" ${logger_outout}/3 "
-		;;
-		"240.0.0.0") logger_output=" ${logger_outout}/4 "
-		;;
-		"248.0.0.0") logger_output=" ${logger_outout}/5 "
-		;;
-		"252.0.0.0") logger_output=" ${logger_outout}/6 "
-		;;
-		"254.0.0.0") logger_output=" ${logger_outout}/7 "
-		;;
-		"255.0.0.0") logger_output=" ${logger_outout}/8 "
-		;;
-		"255.128.0.0") logger_output=" ${logger_outout}/9 "
-		;;
-		"255.192.0.0") logger_output=" ${logger_outout}/10 "
-		;;
-		"255.224.0.0") logger_output=" ${logger_outout}/11 "
-		;;
-		"255.240.0.0") logger_output=" ${logger_outout}/12 "
-		;;
-		"255.258.0.0") logger_output=" ${logger_outout}/13 "
-		;;
-		"255.252.0.0") logger_output=" ${logger_outout}/14 "
-		;;
-		"255.254.0.0") logger_output=" ${logger_outout}/15 "
-		;;
-		"255.255.0.0") logger_output=" ${logger_outout}/16 "
-		;;
-		"255.255.128.0") logger_output=" ${logger_outout}/17 "
-		;;
-		"255.255.192.0") logger_output=" ${logger_outout}/18 "
-		;;
-		"255.255.224.0") logger_output=" ${logger_outout}/19 "
-		;;
-		"255.255.240.0") logger_output=" ${logger_outout}/20 "
-		;;
-		"255.255.248.0") logger_output=" ${logger_outout}/21 "
-		;;
-		"255.255.252.0") logger_output=" ${logger_outout}/22 "
-		;;
-		"255.255.254.0") logger_output=" ${logger_outout}/23 "
-		;;
-		"255.255.255.0") logger_output=" ${logger_outout}/24 "
-		;;
-		"255.255.255.128") logger_output=" ${logger_outout}/25 "
-		;;
-		"255.255.255.192") logger_output=" ${logger_outout}/26 "
-		;;
-		"255.255.255.224") logger_output=" ${logger_outout}/27 "
-		;;
-		"255.255.255.240") logger_output=" ${logger_outout}/28 "
-		;;
-		"255.255.255.248") logger_output=" ${logger_outout}/29 "
-		;;
-		"255.255.255.252") logger_output=" ${logger_outout}/30 "
-		;;
-		"255.255.255.254") logger_output=" ${logger_outout}/31 "
-		;;
-		"255.255.255.255") logger_output=" ${logger_outout}/32 "
-		;;		
-	esac
 	if [ "$logger_output" != "$logger_output1" ]; then
 		logger -t owrtinfo "$logger_output"
 	fi
